eb1685fcc64f91e9c35abc12bf4e99fe59dbc784
Daniel Liao <10663736+liaodaniel@users.noreply.github.com>
2023-11-07 10:15:07 +1100

    PLA-6149: Changeset for TyroTapToPaySDK milestone 1 (0.1.0).

---
+++ SampleApp/SampleApp/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  SampleApp
+//
+//  Created by Christopher Grantham on 1/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView() 
+    }
+  }
+}
