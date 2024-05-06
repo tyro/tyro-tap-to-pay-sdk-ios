85f49b62d68debbc710a08c1bab82576048dc708
Sanjay Narayana <snarayana@tyro.com>
2024-05-06 10:07:33 +1000

    PLA-6312: allow 204 digital receipt

--- Package.swift
+++ Package.swift
@@ -39,8 +39,8 @@ let package = Package(
             ]
     ),
     .binaryTarget(name: "TyroTapToPaySDK",
-                  url: "https://tap-to-pay.connect.tyro.com/tyro/0.9.0/TyroTapToPaySDK.xcframework.zip",
-                  checksum: "23cbf2e352d8c9c8d1f5e56fa4b0ef246b47bfc80df0ff8c0365b1894c6ff4f7"),
+                  url: "https://tap-to-pay.connect.tyro.com/tyro/0.9.1/TyroTapToPaySDK.xcframework.zip",
+                  checksum: "3e0aac45d79a8b3a90284f7f1fba789b9ec6eea44151a56dc68adcda94be67d4"),
     .binaryTarget(name: "MobileConfiguration",
                   url: "https://tap-to-pay.connect.tyro.com/ss/1.0.8.0/SSMobileConfiguration.xcframework.zip",
                   checksum: "ee6a8513480b8c39125eab131e9df5e223d63e3005dbf34b52f36734f719b115"),
