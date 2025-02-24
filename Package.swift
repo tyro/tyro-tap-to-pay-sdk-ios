8042c412dfab19f6c626a593cf6f8111be7d60b3
Sanjay Narayana <snarayana@tyro.com>
2025-02-24 13:18:49 +1100

    try 'latest' version

--- Package.swift
+++ Package.swift
@@ -14,14 +14,12 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "3.1.4000")),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
-    .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
     .package(url: "https://github.com/microsoft/plcrashreporter.git", from: "1.11.2")
   ],
   targets: [
     .target(name: "TyroTapToPaySDKPackage",
             dependencies: [
               .product(name: "OpenSSL", package: "OpenSSL"),
-              .product(name: "KeychainAccess", package: "KeychainAccess"),
               .product(name: "CrashReporter", package: "plcrashreporter"),
               .target(name: "TyroTapToPaySDK"),
               .target(name: "MobileMPOSSDK"),
